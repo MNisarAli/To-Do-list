429481f8f62ce969f383487c2f14dfe3d30454a7
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2022-12-17 14:44:44 +0500

    Add eventListener to the checkbox

--- src/userActions.js
+++ src/userActions.js
@@ -1,3 +1,14 @@
 export default class UserActions {
-
+  // Mark Task As Completed & Update Its Object's Value For Completed Key.
+  completeTask = (items) => {
+    const task = document.querySelectorAll('.label-container');
+    const checkboxes = document.querySelectorAll('input[type=checkbox]');
+    const toDo = document.querySelectorAll('.to-do');
+    checkboxes.forEach((checkbox, index) => {
+      checkbox.addEventListener('click', () => {
+        task[index].classList.toggle('opacity');
+        toDo[index].classList.toggle('decoration');
+      });
+    });
+  }
 }
