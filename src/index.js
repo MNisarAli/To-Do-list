986344f44f80aa7cb2f02f7d48a41ac34a0376b9
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2022-12-17 14:54:53 +0500

    Fix lint errors

--- src/index.js
+++ src/index.js
@@ -3,14 +3,5 @@ import Tasks from './taskUpdate.js';
 
 const tasks = new Tasks();
 
-// Add New Task In List.
-const newInput = document.querySelector('#new-input');
-newInput.addEventListener('keypress', (e) => {
-  if (e.key === 'Enter' && newInput.value) {
-    tasks.addTask(newInput.value);
-    newInput.value = '';
-  }
-});
-
 // On Page Load Display Tasks From Local Storage.
 window.addEventListener('DOMContentLoaded', () => tasks.displayList());
