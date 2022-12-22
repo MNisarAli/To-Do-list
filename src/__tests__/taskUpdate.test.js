0269effa64e347a8f4c291d129169c9a86532f18
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2022-12-22 16:00:56 +0500

    Setup jest, babel, and mocks for testing

---
+++ src/__tests__/taskUpdate.test.js
@@ -0,0 +1,9 @@
+import Tasks from "../modules/taskUpdate";
+
+const tasks = new Tasks;
+
+describe("Task Update actions testing", () => {
+  test("Should check for adding task list item", () => {
+    
+  });
+});
