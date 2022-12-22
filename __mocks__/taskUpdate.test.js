8b3b5b35fda995fad64af484d5c4f7231bed538c
mutai-gilbert <mutaigilbert85@gmail.com>
2022-12-22 19:28:16 +0300

    Made changes to the mocks folder

---
+++ __mocks__/taskUpdate.test.js
@@ -0,0 +1,48 @@
+import tasks from './taskUpdate.js';
+
+global.localStorage = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn(),
+};
+
+it('should call the setItem function of the localStorage object', () => {
+  tasks.toDoList = [{ description: 'Task 1', completed: false, index: 0 }];
+  global.localStorage.setItem = jest.fn();
+  tasks.setLocalStorage();
+  expect(global.localStorage.setItem).toHaveBeenCalledWith('tasks', JSON.stringify(tasks.toDoList));
+});
+
+describe('tasks', () => {
+  describe('addTask', () => {
+    it('should add a new object to the toDolist array', () => {
+      tasks.toDoList = [];
+      const value = 'Task 1';
+      tasks.addTask(value);
+      expect(tasks.toDoList).toHaveLength(1);
+    });
+    it('should add a new object with the correct properties to the toDolist array', () => {
+      tasks.toDoList = [];
+      const value = 'Task 1';
+      tasks.addTask(value);
+      expect(tasks.toDoList[0]).toHaveProperty('description', value);
+      expect(tasks.toDoList[0]).toHaveProperty('completed', value);
+      expect(tasks.toDoList[0]).toHaveProperty('index', value);
+    });
+    it('should call the localStorage function', () => {
+      tasks.toDoList = [];
+      const value = 'Task 1';
+      tasks.setLocalStorage = jest.fn();
+      tasks.addTask(value);
+      expect(tasks.setLocalStorage).toHaveBeenCalled();
+    });
+    it('should call the displayList function', () => {
+      tasks.toDoList = [];
+      const value = 'Task 1';
+      tasks.displayList = jest.fn();
+      tasks.addTask(value);
+      expect(tasks.displayList).toHaveBeenCalled();
+    });
+  });
+});
--- src/__tests__/taskUpdate.test.js
+++
@@ -1,9 +0,0 @@
-import Tasks from "../modules/taskUpdate";
-
-const tasks = new Tasks;
-
-describe("Task Update actions testing", () => {
-  test("Should check for adding task list item", () => {
-    
-  });
-});
