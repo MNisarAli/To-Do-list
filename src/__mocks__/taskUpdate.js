13a659e200b648c333710520d47f4ead999a1739
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2022-12-23 18:42:06 +0500

    Removed Unnecessary commented out code

--- src/__mocks__/taskUpdate.js
+++ src/__mocks__/taskUpdate.js
@@ -10,9 +10,6 @@ global.document = dom.window.document;
 const userAction = new UserActions();
 
 export default class Tasks {
-  // constructor() {
-  //   this.toDoList = JSON.parse(localStorage.getItem('tasks')) || [];
-  // }
 
   // Function displayTasks.
   displayList = () => {
